6dc415965c098dee522a7433fbac5e3ac8697197
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-31 01:56:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  attr_accessor :
+  attr_accessor : :hair_color, :eye_color, 
+      :height
+      :weight => "good",
+      :handed => "lefty",
+      :complexion => "decent",
+      :t_shirt_size => "medium",
+      :wrist_size => "small",
+      :glove_size => "normal",
+      :pant_length => "32",
+      :pant_width => "32"
 end
